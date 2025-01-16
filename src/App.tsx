3ffdefb5c38b3af08467d110b934e8cbde9772ef
David Seow <david.seow@johnlewis.co.uk>
2025-01-16 13:56:24 +0000

    fix build error - remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import StickyBoard from './components/stickyBoardComponent/stickyBoardComponent'
 import './App.css'
 
