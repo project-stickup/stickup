e8b9da228dbed5bc9ae595f9f009dd066c884eb1
David Seow <david.seow@johnlewis.co.uk>
2025-02-13 17:50:42 +0000

    websocket emit and listen to event

--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
-import http from 'http';
-import { Server } from 'socket.io';
+import http from "http";
+import { Server } from "socket.io";
 
+const server = http.createServer(() => {});
+const io = new Server(server, { cors: { origin: "*" } });
 
-const server = http.createServer(() => {
-})
-const io = new Server(server, {cors: {origin: '*'}});
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-io.on('connection', (socket) => {
-  console.log('socket', socket)
-  console.log('a user connected');
+  // post message to current web socket room
+  socket.on("testEvent", (msg) => {
+    console.log(`server received ${msg}`);
+    socket.broadcast.emit("serverTestEvent", msg);
+  });
 });
-
 
 const port = 8080;
 server.listen(port, () => {
-
-  console.log('Server is live and direct....')
-})
+  console.log("Server is live and direct....");
+});
